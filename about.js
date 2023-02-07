68464a995666e6669cd9c7f19cb89ee64f6ca576
Ritika Rawat <Ritikarawat220@gmail.com>
2023-02-07 18:34:48 +0530

    Modified desktop view

--- about.js
+++ about.js
@@ -2,16 +2,16 @@ const menuList = document.querySelector('.menu-sec');
 const menuIcon = document.querySelector('.menu');
 
 menuIcon.addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu')) {
-        menuList.style.display = 'flex';
-        menuIcon.style.display = 'none';
-    }
+  if (e.target.classList.contains('menu')) {
+    menuList.style.display = 'flex';
+    menuIcon.style.display = 'none';
+  }
 });
 
 menuList.addEventListener('click', (e) => {
-    const clicked = e.target.classList.contains('links');
-    if (clicked) {
-        menuList.style.display = 'none';
-        menuIcon.style.display = 'flex';
-    }
+  const clicked = e.target.classList.contains('links');
+  if (clicked) {
+    menuList.style.display = 'none';
+    menuIcon.style.display = 'flex';
+  }
 });
--- main.js
+++ main.js
@@ -3,74 +3,74 @@ const menuIcon = document.querySelector('.menu');
 const allSpeakers = document.querySelector('.speakers');
 
 menuIcon.addEventListener('click', (e) => {
-    if (e.target.classList.contains('menu')) {
-        menuList.style.display = 'flex';
-        menuIcon.style.display = 'none';
-    }
+  if (e.target.classList.contains('menu')) {
+    menuList.style.display = 'flex';
+    menuIcon.style.display = 'none';
+  }
 });
 
 menuList.addEventListener('click', (e) => {
-    const clicked = e.target.classList.contains('links');
-    if (clicked) {
-        menuList.style.display = 'none';
-        menuIcon.style.display = 'flex';
-    }
+  const clicked = e.target.classList.contains('links');
+  if (clicked) {
+    menuList.style.display = 'none';
+    menuIcon.style.display = 'flex';
+  }
 });
 
 const guests = [
-    {
-        Name: 'RENATA QUINTINI',
-        Title: 'FOUNDER AT RENEGADE PARTNERS',
-        Description: 'Renata is a co-founder of Renegade Partners. Renata was a partner at early-stage deep tech investment firm Lux Capital.',
-        Img: './SPEAKERS/speaker1.avif',
-        Alt: 'speaker1',
-    },
+  {
+    Name: 'RENATA QUINTINI',
+    Title: 'FOUNDER AT RENEGADE PARTNERS',
+    Description: 'Renata is a co-founder of Renegade Partners. Renata was a partner at early-stage deep tech investment firm Lux Capital.',
+    Img: './SPEAKERS/speaker1.avif',
+    Alt: 'speaker1',
+  },
 
-    {
-        Name: 'HANS TUNG',
-        Title: 'MANAGING PARTNER AT GGV CAPITAL',
-        Description: 'Hans is consistently recognized among the top VC investors in the world, and has been named to the Forbes Midas list every year, most recently ranking #3.',
-        Img: './SPEAKERS/speaker2.avif',
-        Alt: 'speaker2',
-    },
+  {
+    Name: 'HANS TUNG',
+    Title: 'MANAGING PARTNER AT GGV CAPITAL',
+    Description: 'Hans is consistently recognized among the top VC investors in the world, and has been named to the Forbes Midas list every year, most recently ranking #3.',
+    Img: './SPEAKERS/speaker2.avif',
+    Alt: 'speaker2',
+  },
 
-    {
-        Name: 'THOMAS DOHMKE',
-        Title: 'CEO AT GITHUB',
-        Description: 'Thomas holds a PhD in mechanical engineering, and enjoys coding and making GitHub contribution graphs green.',
-        Img: './SPEAKERS/extra.avif',
-        Alt: 'speaker3',
-    },
+  {
+    Name: 'THOMAS DOHMKE',
+    Title: 'CEO AT GITHUB',
+    Description: 'Thomas holds a PhD in mechanical engineering, and enjoys coding and making GitHub contribution graphs green.',
+    Img: './SPEAKERS/extra.avif',
+    Alt: 'speaker3',
+  },
 
-    {
-        Name: 'JEFF SHINER',
-        Title: 'CEO AT 1PASSWORD',
-        Description: 'Jeff is the CEO of 1Password, a leader in human-centric security and privacy.',
-        Img: './SPEAKERS/speaker4e.avif',
-        Alt: 'speaker4',
-    },
+  {
+    Name: 'JEFF SHINER',
+    Title: 'CEO AT 1PASSWORD',
+    Description: 'Jeff is the CEO of 1Password, a leader in human-centric security and privacy.',
+    Img: './SPEAKERS/speaker4e.avif',
+    Alt: 'speaker4',
+  },
 
-    {
-        Name: 'CATHERINE POWELL',
-        Title: 'GLOBAL HEAD OF HOSTING AT AIRBNB',
-        Description: 'Catherine is the global head of hosting at Airbnb, where she leads the team that supports hosts around the world.',
-        Img: './SPEAKERS/speak5.avif',
-        Alt: 'speaker5',
-    },
+  {
+    Name: 'CATHERINE POWELL',
+    Title: 'GLOBAL HEAD OF HOSTING AT AIRBNB',
+    Description: 'Catherine is the global head of hosting at Airbnb, where she leads the team that supports hosts around the world.',
+    Img: './SPEAKERS/speak5.avif',
+    Alt: 'speaker5',
+  },
 
-    {
-        Name: 'ERIC SCHMIDT',
-        Title: 'CO-FOUNDER AT SCHMIDT FUTURES',
-        Description: 'Eric is an accomplished technologist, entrepreneur, author and philanthropist. He served as Google’s chairman and CEO',
-        Img: './SPEAKERS/speaker6.avif',
-        Alt: 'speaker6',
-    },
+  {
+    Name: 'ERIC SCHMIDT',
+    Title: 'CO-FOUNDER AT SCHMIDT FUTURES',
+    Description: 'Eric is an accomplished technologist, entrepreneur, author and philanthropist. He served as Google’s chairman and CEO',
+    Img: './SPEAKERS/speaker6.avif',
+    Alt: 'speaker6',
+  },
 ];
 
 function guestSpeakers() {
-    let speakerOut = '';
-    for(let i = 0; i < guests.length; i += 1) {
-        speakerOut += `
+  let speakerOut = '';
+  for (let i = 0; i < guests.length; i += 1) {
+    speakerOut += `
         <li class="guest">
         <img src="${guests[i].Img}" alt="${guests[i].Alt}">
         <section>
@@ -85,9 +85,9 @@ function guestSpeakers() {
         </p>
         </section>
         </li>`;
-    }
+  }
 
-    allSpeakers.innerHTML = speakerOut;
+  allSpeakers.innerHTML = speakerOut;
 }
 
 guestSpeakers();
